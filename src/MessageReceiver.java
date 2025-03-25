b3597a04d4b2037c4c821eea5a281a3b320ffa2e
Pratham Reet <prathamreet@gmail.com>
2025-03-25 21:20:27 +0530

    del

--- src/MessageReceiver.java
+++
@@ -1,3 +0,0 @@
-public interface MessageReceiver {
-    void receiveMessage(String message);
-} 
--- src/Utils.java
+++
@@ -1,5 +0,0 @@
-public class Utils {
-    public static String formatMessage(String nickname, String message) {
-        return nickname + ": " + message;
-    }
-}
